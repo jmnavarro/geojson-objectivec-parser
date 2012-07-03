7dee45f6f390545fede125687f180c78a8219ebd
JM <jmnavarro@gmail.com>
2012-07-03 16:38:47 +0200

    sample project

---
+++ GeoJSONParserSample/GeoJSONParserSample/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  GeoJSONParserSample
+//
+//  Created by JM on 03/07/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ GeoJSONParserSample/GeoJSONParserSample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GeoJSONParserSample
+//
+//  Created by JM on 03/07/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
